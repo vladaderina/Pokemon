4b6334267c63ff82f423543a6b18ac561839fafd
vladaderina <96880544+vladaderina@users.noreply.github.com>
2023-06-29 10:09:31 +0500

    Add files via upload

---
+++ itog.py
@@ -0,0 +1,20 @@
+import requests
+import json
+def GetInfo(ID):
+    try:
+        pokemonInfoJS = requests.get(f'https://pokeapi.co/api/v2/pokemon/{ID}')
+        if "name" not in pokemonInfoJS.text:
+            raise NameError("Отсутствует информация о имени")
+        elif "ability" not in pokemonInfoJS.text:
+            raise NameError("Отсутствует информация о способностях")
+        elif pokemonInfoJS.headers['Content-Type'] != 'application/json; charset=utf-8':
+            raise TypeError("Файл имеет не JSON формат")
+        pokemonInfoDC = json.loads(pokemonInfoJS.content)
+        ab = list()
+        for it in pokemonInfoDC['abilities']:
+            ab.append(it['ability']['name'])
+        return (f"Имя покемона: {pokemonInfoDC['name']}, его способности: {ab}")
+    except Exception as e:
+        raise Exception("Покемона с таким ID нет")
+if __name__ == '__main__':
+    print(GetInfo("56"))
---
+++ unit_test.py
@@ -0,0 +1,12 @@
+from unittest import TestCase, main
+from itog import GetInfo
+
+class CalcTest(TestCase):
+    def test_posit(self):
+        self.assertEqual(GetInfo("56"), "Имя покемона: mankey, его способности: ['vital-spirit', 'anger-point', 'defiant']")
+    def test_notname(self):
+        with self.assertRaises(Exception) as e:
+            GetInfo("dd")
+        self.assertEqual('Покемона с таким ID нет', e.exception.args[0])
+if __name__ == '__main__':
+    main()
